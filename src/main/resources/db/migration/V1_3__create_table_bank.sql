c7f0b8767dc514fc80b7e3a7825c56932e7ccc78
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-07-21 06:01:09 -0300

    Create state, modifeid agency, create city, modifeid bank

--- src/main/resources/db/migration/V1_3__create_table_bank.sql
+++ src/main/resources/db/migration/V1_3__create_table_bank.sql
@@ -1,10 +1,13 @@
 create table if not exists bank(
     id_bank bigint(20) not null primary key auto_increment,
     name_bank varchar(70) not null,
+    acronym_bank varchar(5) not null,
     doc_fee decimal(10, 2) default 0,
     ted_fee decimal(10, 2) default 0,
     monthly_free_transfers bigint(3) default 0,
-    status_data_base varchar(8) default 'ACTIVE'
+    rf_agency bigint(20) not null,
+    status_data_base varchar(8) default 'ACTIVE',
+    foreign key
 )engine innoDB default charset=utf8;
 
 alter table bank auto_increment = 001;
