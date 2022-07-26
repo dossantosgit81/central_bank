ba0315fe787a82a5e518469837c4f234bda59948
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-07-26 06:02:48 -0300

    add zerofill

--- src/main/resources/db/migration/V1_13__create_table_account.sql
+++ src/main/resources/db/migration/V1_13__create_table_account.sql
@@ -1,4 +1,5 @@
 create table if not exists account(
-    id_account bigint(20) not null primary key auto_increment,
-    number_account
+    id_account int(12) not null primary key,
+    type_account varchar(15) not null,
+    balance_account decimal(10, 2) default 0;
 );
--- src/main/resources/db/migration/V1_6__create_table_bank.sql
+++ src/main/resources/db/migration/V1_6__create_table_bank.sql
@@ -1,10 +1,10 @@
 create table if not exists bank(
-    id_bank bigint(20) not null primary key auto_increment,
+    id_bank bigint(20) zerofill not null primary key auto_increment,
     name_bank varchar(70) not null,
     acronym_bank varchar(5) not null,
     doc_fee decimal(10, 2) default 0,
     ted_fee decimal(10, 2) default 0,
     monthly_free_transfers bigint(3) default 0,
     status_data_base varchar(8) default 'ACTIVE',
-    thirst_bank bigint(20) null unique
+    thirst_bank int(4) zerofill null unique
 )engine innoDB default charset=utf8;
